f9f046b2ffd67d29fc3b40979024c8676155bb9f
Ebrahim <Ebrahim@DESKTOP-5ND39I7>
2021-03-10 11:36:14 +0330

    02-Add Labale hangman

--- Hangman Gmae/FrmMain.Designer.cs
+++ Hangman Gmae/FrmMain.Designer.cs
@@ -32,6 +32,7 @@
             this.panel1 = new System.Windows.Forms.Panel();
             this.btnExit = new System.Windows.Forms.Button();
             this.btnLoad = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
             this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -54,7 +55,7 @@
             this.panel1.Controls.Add(this.btnExit);
             this.panel1.Controls.Add(this.btnLoad);
             this.panel1.Controls.Add(this.btnNew);
-            this.panel1.Location = new System.Drawing.Point(187, 77);
+            this.panel1.Location = new System.Drawing.Point(187, 182);
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(316, 239);
             this.panel1.TabIndex = 1;
@@ -85,13 +86,25 @@
             this.btnLoad.UseVisualStyleBackColor = false;
             this.btnLoad.Click += new System.EventHandler(this.btnLoad_Click);
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Yellow;
+            this.label1.Location = new System.Drawing.Point(185, 68);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(318, 73);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Hangman";
+            // 
             // FrmMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(6)))), ((int)(((byte)(20)))), ((int)(((byte)(43)))));
-            this.ClientSize = new System.Drawing.Size(689, 444);
+            this.ClientSize = new System.Drawing.Size(689, 514);
             this.ControlBox = false;
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.panel1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "FrmMain";
@@ -99,6 +112,7 @@
             this.Text = "Form1";
             this.panel1.ResumeLayout(false);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -108,6 +122,7 @@
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Button btnExit;
         private System.Windows.Forms.Button btnLoad;
+        private System.Windows.Forms.Label label1;
     }
 }
 
